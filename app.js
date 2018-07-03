25aaeeab4f499b70aa4d383ab071d06a913fe146
windyakin <windyakin@gmail.com>
2018-07-03 20:14:10 +0900

    :camera: Take and save screenshot

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const puppeteer = require('puppeteer');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
+const moment = require('moment');
 
 require('dotenv').config();
 
 const WEBSITE_URL = Buffer.from('aHR0cHM6Ly9sb3ZlbGl2ZS1hcW91cnNjbHViLmpwLw==', 'base64').toString();
 const COOKIES_PATH = `${__dirname}/cookie/data.json`;
+const SCREENSHOTS_PATH = `${__dirname}/screenshots`;
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
@@ -21,6 +23,18 @@ const COOKIES_PATH = `${__dirname}/cookie/data.json`;
   } catch (err) {
     console.error('Failed load cookies', err);
   }
+
+  page.emulate({
+    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    viewport: {
+      width: 1920,
+      height: 1080,
+      deviceScaleFactor: 1,
+      isMobile: false,
+      hasTouch: false,
+      isLandscape: false,
+    },
+  });
 
   await page.goto(WEBSITE_URL, { waituntil: 'networkidle0' });
 
@@ -43,6 +57,12 @@ const COOKIES_PATH = `${__dirname}/cookie/data.json`;
     await page.close();
     await browser.close();
     process.exit(1);
+  }
+
+  try {
+    await page.screenshot({ fullPage: true, path: `${SCREENSHOTS_PATH}/${moment().format('YYYYMMDDHHmmss')}.png` });
+  } catch (err) {
+    console.error('Failed take screenshot', err);
   }
 
   await fs.writeFileAsync(COOKIES_PATH, JSON.stringify(await page.cookies()));
